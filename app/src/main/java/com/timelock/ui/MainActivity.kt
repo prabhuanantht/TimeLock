90097b176096b3e8814ad2058783106d1de8135a
Ananth Prabhu T <2022is_ananthprabhut_a@nie.ac.in>
2026-02-11 20:09:17 +0530

    Phase 1: Project Setup

---
+++ app/src/main/java/com/timelock/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.timelock.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.timelock.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
